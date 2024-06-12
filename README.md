f1fa8c08977d39df0a9ede0fabff73402a520863
rishishanbhag <rishi.shanbhag2024@gmail.com>
2024-06-13 00:54:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# drum_kit
+ a normal drum kit which produces different sounds when  the user clicks different buttons.Made by using plain HTML,CSS and Javascript.
---
+++ drum_kit/index.js
@@ -0,0 +1,63 @@
+var x=document.querySelectorAll(".drum").length;
+
+
+for(var i=0;i<x;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",handleclick);
+}
+
+
+// here what we have done is
+// we have applied a for loop on the buttons and then when clicked we have applied an eventlistener
+//  to it which gives us a  prompt alert.
+
+
+function handleclick(){
+    // alert("button was clicked");
+    // this.style.color="white";
+    var btn=this.innerHTML;
+    switch (btn) {
+        case "w":
+            var v1= new Audio("clash.mp3");
+            v1.play();  
+            break; 
+
+        case "a":
+            var v2= new Audio("drums.mp3");
+            v2.play();  
+            break; 
+
+        case "s":
+            var v3= new Audio("base.mp3");
+            v3.play();  
+            break;
+
+
+        case "d":
+            var v4= new Audio("electric-guitar-metal-riff-107087.mp3");
+            v4.play();  
+            break;    
+        
+        case "j":
+            var v5= new Audio("ting.mp3");
+            v5.play();  
+            break;
+
+        case "k":
+            var v6= new Audio("flute.mp3");
+            v6.play();  
+            break;
+    
+        case "l":
+            var v7= new Audio("snare.mp3");
+            v7.play();  
+            break;
+
+
+
+        default:
+            console.log("done");
+    }
+
+}
+
+
